ed9cfe47a8f96fe79d2193fb453a219cefbce52b
Anne Guichard <anne.guichard@irisa.fr>
2020-11-13 18:45:27 +0100

    style(olc.py): improve the code formatting using the PEP8 norm

--- olc.py
+++ olc.py
@@ -1,109 +1,106 @@
+"""Module 'olc.py': development of the script OLC
+
+The module 'olc.py' contains the pipeline of the gap-filling using an OLC method.
+"""
 #!/usr/bin/env python3
 from __future__ import print_function
 import os
 import sys
-import re
-import subprocess
-import time
-from Bio import SeqIO
-from Bio.Seq import Seq
 from operator import itemgetter
+from Bio.Seq import Seq
+from main import START, STOP, input_seqName, list_of_abundance_min, readList, assembly_file
+from helpers import index_read, extend
+'''
 from helpers import Graph, index_read, extend
-from main import start, stop, input_seqName, s, o_min, list_of_a, max_length, readList, assembly_file
+'''
 
-#Augmenter la taille maximale de la pile de recursion en Python:
+# Increase the maximum recursion depth in Python.
 sys.setrecursionlimit(50000)
 
 
 #----------------------------------------------------
 # Gapfilling with Seed-and-Extend approach
 #----------------------------------------------------
-'''
+"""Three main variables are used in this pipeline:
+
 readList = list of all reads' sequences
-seedDict = dictionary containing the seed's sequence as key, and the list of positions of reads having this seed in readList as value (-pos if revcomp of read)
-readWithStart = list of all reads containing the full sequence of the kmer start, along with the index of the beginning of the kmer start's subsequence, 
+seedDict = dictionary containing the seed's sequence as key, and the list of positions (i) of reads having this seed in readList as value (-pos if revcomp of read)
+readWithStart = list of all reads containing the full sequence of the kmer start, along with the index of the beginning of the kmer start's subsequence,
                 referenced as a sublist of the readWithStart list: [position of the read in readList, index of beginning of kmer start's subsequence]
-'''
+"""
 try:
-    startTime = time.time()
+    # Initiate the three main variables.
+    seedDict = {}
+    readWithStart = []
+    pos_read_in_readList = 0
 
-    #Gap-filling with a specific abundance threshold
-    for a in list_of_a:
+    # Iterate over the reads of 'readList' to obtain the 'seedDict' dictionary and the 'readWithStart' list.
+    for read in readList:
+        # Get the reverse complement of the current read.
+        read_rc = str(Seq(read).reverse_complement())
+        # Seed the read and update the 'seedDict' dictionary.
+        index_read(read, pos_read_in_readList, read_rc, seedDict)
+        # Search if the read contains the whole kmer START's sequence and update the 'readWithStart' list.
+        if START in read:
+            readWithStart.append([str(pos_read_in_readList), read.index(START)])
+        elif START in read_rc:
+            readWithStart.append(["-"+str(pos_read_in_readList), read_rc.index(START)])
+        # Increment the position of the current read in 'readList'
+        pos_read_in_readList += 1
 
-        seedDict = {}                                   #value of dict is a list of position (i) of reads in readList (-pos if revcomp of read)
-        readWithStart = []
-        pos_read_in_readList = 0
+    # Sort the 'readWithStart' list by the minimum extension size (e.g. by the maximum index).
+    readWithStart = sorted(readWithStart, key=itemgetter(1), reverse=True)
+    # If there is no read containing the kmer start, raise an exception.
+    if not readWithStart:
+        print("\nNo read in the dataset provided contains the kmer start... \nHence, tentative of gapfilling aborted...")
+        sys.exit(1)
 
-        for read in readList:
-            #Get the reverse complement of the read
-            read_rc = str(Seq(read).reverse_complement())
-
-            #Seed the read and update the dictionary of the seeds of the reads
-            index_read(read, pos_read_in_readList, read_rc, seedDict)
-
-            #Search if the read contains the whole kmer start's sequence and update the readWithStart list
-            if start in read:
-                readWithStart.append([str(pos_read_in_readList), read.index(start)])
-            elif start in read_rc:
-                readWithStart.append(["-"+str(pos_read_in_readList), read_rc.index(start)])
-
-            pos_read_in_readList += 1
-
-        #Sort the readWithStart list by the minimum extension size (e.g. the maximum index)
-        readWithStart = sorted(readWithStart, key=itemgetter(1), reverse=True)
-        if len(readWithStart) == 0:
-            print("\nNo read in the dataset provided contains the kmer start... \nHence, tentative of gapfilling aborted...")
-            sys.exit(1)
-
-        #Create graph "à la volée"
+    # Create graph "a la volee".
         '''
         nodes = {}
         graph = Graph(nodes)
         graph.add_node(start)
         '''
 
-        #Extend the reads containing the whole kmer start's sequence
-        for (pos_read, index) in readWithStart:
-            #get the sequence of the read
-            if '-' in str(pos_read):
-                read = str(Seq(readList[int(pos_read.split('-')[1])]).reverse_complement())
-            else:
-                read = readList[int(pos_read)]
+    # Extend the reads containing the whole kmer start's sequence.
+    for (pos_read, index) in readWithStart:
 
-            #update the graph with this read
-            '''
-            graph.add_node(read)
-            graph.add_edge((start, read, 0))
-            '''
+        # Get the sequence of the read.
+        if '-' in str(pos_read):
+            read = str(Seq(readList[int(pos_read.split('-')[1])]).reverse_complement())
+        else:
+            read = readList[int(pos_read)]
 
-            #Extend the assembly sequence
-            res, success = extend(read, len(read), a, seedDict)
-            '''
-            res, success = extend(read, read, a, seedDict, graph)
-            '''
-            if not success:
-                print(res)
-                solution = False
-                
-            if success:
-                print("\nAbundance threshold value: {} \nSuccessful Gapfilling !".format(a))
-                #Save the gapfilled sequence in the output_file
-                with open(assembly_file, "a") as assemblyFile:           #TODO: append or write ?
-                    assembly_startbeg = res.index(start)
-                    assembly_stopbeg = res.index(stop)
-                    seq = res[assembly_startbeg:assembly_stopbeg+len(stop)]
-                    seq_name = "assembly." + input_seqName + "_a" + str(a) + " len " + str(len(seq))
-                    assemblyFile.write(">" + seq_name)
-                    assemblyFile.write("\n" + seq + "\n")
-                    solution = True
-                break
+        # Update the graph with this read.
+        '''
+        graph.add_node(read)
+        graph.add_edge((start, read, 0))
+        '''
 
-        #Iterate over the values of abundance_min ONLY if no solution is found
-        if solution == True:
+        # Extend the assembly sequence (e.g. the current read containing the whole kmer start's sequence) using the function 'extend()'
+        for abundance_min in list_of_abundance_min:
+            res, success = extend(read, len(read), abundance_min, seedDict)
+            '''
+            res, success = extend(read, len(read), abundance_min, seedDict, graph)
+            '''
+        # Case of unsuccessful gap-filling.
+        if not success:
+            print(res)
+            solution = False
+        # Case of successful gap-filling.
+        if success:
+            print("\nSuccessful Gapfilling !")
+            # Save the gap-filled sequence in the output_file.
+            with open(assembly_file, "a") as assemblyFile:
+                assembly_startbeg = res.index(START)
+                assembly_stopbeg = res.index(STOP)
+                seq = res[assembly_startbeg:assembly_stopbeg+len(STOP)]
+                seq_name = "assembly." + input_seqName + "_a" + str(abundance_min) + " len " + str(len(seq))
+                assemblyFile.write(">" + seq_name)
+                assemblyFile.write("\n" + seq + "\n")
+                solution = True
+            # Iterate over the values of abundance_min only if no solution is found.
             break
-
-    endTime = time.time()
-    print("It took %f seconds" %(endTime-startTime))
 
 
 except Exception as e:
@@ -113,10 +110,3 @@ except Exception as e:
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     print(exc_type, fname, exc_tb.tb_lineno)
     sys.exit(1)
-
-
-
-
-#TODO: if readWithStart empty, search for reads overlapping towards the right, with min overlap (especially if long start)
-    #--> modif lines 47-49 of olc.py
-#TODO: in future: try with multiprocessing
